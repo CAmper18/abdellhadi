786ed38dfb2257a6d2193f8bf888b37bd8fa5d6c
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-29 05:21:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3416,7 +3416,27 @@ client.on('message', msg => {
 
 
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'stylight');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('🎽 | name :  ',`${member}`)
+        .addField('📢 | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SERVER NAME **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
 
 
 
